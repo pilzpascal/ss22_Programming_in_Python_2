646cd5be827376e8670e930e3af1d162e33bd80d
innenseite <40318038+innenseite@users.noreply.github.com>
2022-04-28 11:26:18 +0200

    Debugging mostly done
    Mostly everything seems to work, only the target_array is not as expected. The unittest gives 17.10 points

--- 2/exercise 4/ex4.py
+++ 2/exercise 4/ex4.py
@@ -15,24 +15,28 @@ def ex4(image_array, offset, spacing):
     # Checking the other rules that the assignment specifies
     if not isinstance(image_array, np.ndarray):
         raise TypeError(f"image_array is of type {type(image_array)}, not np.ndarray.")
-    if not (image_array.shape[2] == 3 or image_array.ndim == 3):
+    if not (image_array.ndim == 3 and image_array.shape[2] == 3):
         raise NotImplementedError("image_array is not a 3D array.")
     for i in offset:
-        if i < 0 or i > 32:
+        if not 0 <= i <= 32:
             raise ValueError("offset is not in [0,32].")
     for i in spacing:
-        if i < 2 or i > 8:
+        if not 2 <= i <= 8:
             raise ValueError("Spacing is not in [2,8].")
     # We check if the number of remaining pixels is grater than 144
     # The formula I use for this is
     # ((length_of_rows - offset_rows) / spacing_rows) * ((length_of_cols - offset_cols) / spacing_cols)
-    if ceil((image_array.shape[1] - offset[0]) / spacing[0]) *\
-            ceil((image_array.shape[0] - offset[1]) / spacing[1]) < 144:
+    if not ceil((image_array.shape[1] - offset[0]) / spacing[0]) *\
+            ceil((image_array.shape[0] - offset[1]) / spacing[1]) >= 144:
         raise ValueError(f"The number of known pixels after removing must be at least 144 but is \
 {ceil((image_array.shape[1] - offset[0]) / spacing[0]) * ceil((image_array.shape[0] - offset[1]) / spacing[1])}")
 
+    # We now have input_array.shape = (M, N, 3)
+
     # Creating a working copy of the image, which also transposed immediately
     input_array = np.transpose(image_array, (2, 0, 1)).copy()
+
+    # After the transpose we now have input_array.shape = (3, M, N)
 
     # Setting the offset rows and cols to 0
     # N
@@ -41,16 +45,16 @@ def ex4(image_array, offset, spacing):
     input_array[:, :offset[1], :] = 0
 
     # N
-    for i in range(offset[0] + 1, input_array.shape[2]):
+    for i in range(input_array.shape[2] - offset[0]):
         # With this condition we make every row that is not a multiple of the row spacing to 0
-        if not i % spacing[0] == 1:
-            input_array[:, :, i] = 0
+        if not i % spacing[0] == 0:
+            input_array[:, :, (i + offset[0])] = 0
 
     # M
-    for i in range(offset[1] + 1, input_array.shape[1]):
+    for i in range(input_array.shape[1] - offset[1]):
         # With this condition we make every col that is not a multiple of the col spacing to 0
-        if not i % spacing[1] == 1:
-            input_array[:, i, :] = 0
+        if not i % spacing[1] == 0:
+            input_array[:, (i + offset[1]), :] = 0
 
     # We get the array of known pixels, represented by 0 if unknown, 1 if known.
     # This should be of same shape as image_array, therefore we transpose again.
@@ -58,6 +62,6 @@ def ex4(image_array, offset, spacing):
     known_array[known_array > 0] = 1
 
     # target_array is equal to the image array
-    target_array = image_array[known_array < 1].flatten().copy()
+    target_array = image_array[known_array < 1].flatten()
 
     return input_array, known_array, target_array
